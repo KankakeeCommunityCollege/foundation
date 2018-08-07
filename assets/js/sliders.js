2db1bfd63a4c963d2acf20db5ca1aa18b32760c6
wdzajicek <tig.welder4@gmail.com>
2018-08-07 16:57:50 -0500

    Add lazyLoad,js and sliders.js from athletics site.

---
+++ assets/js/sliders.js
@@ -0,0 +1,11 @@
+// Custom JS for Slick img carousels
+
+$('.heroSlider').slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  prevArrow:'<img alt="" class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
+  nextArrow:'<img alt="" class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
+});
