3297f6358a5b4a763123eb921284edb5b2dbad0e
wdzajicek <tig.welder4@gmail.com>
2018-08-31 15:28:27 -0500

    Remove box shaddow from shop amazon smile widget's tab when minimized.

--- assets/js/script/shopSmile.js
+++ assets/js/script/shopSmile.js
@@ -4,16 +4,16 @@ $(document).ready(function () {
 
   var smileWrapper = $('#shopSmileWrapper');
 
-  function addShaddow() {
-    var smileTab = $('#smileTab');
-    setTimeout(function(){
-      smileTab.addClass('shop-smile__tab--shaddow');
-    }, 1200);
-  }
-  function removeShaddow() {
-    var smileTab = $('#smileTab');
-    smileTab.removeClass('shop-smile__tab--shaddow');
-  }
+  //function addShaddow() {
+  //  var smileTab = $('#smileTab');
+  //  setTimeout(function(){
+  //    smileTab.addClass('shop-smile__tab--shaddow');
+  //  }, 1200);
+  //}
+  //function removeShaddow() {
+  //  var smileTab = $('#smileTab');
+  //  smileTab.removeClass('shop-smile__tab--shaddow');
+  //}
 
   function animateIn() {
     // Function to animate the slide-in of the amazon smile slide-box
@@ -35,7 +35,7 @@ $(document).ready(function () {
   // Run the slide-box animation on page-load after a delay:
   function showShopSmile() {
     setTimeout(function(){
-      removeShaddow();
+      //removeShaddow();
       animateIn();
     }, 2000);
   }
@@ -53,7 +53,7 @@ $(document).ready(function () {
       //$('#greyOut').removeClass('shop-smile__grey-out--animate')
       //  .addClass('shop-smile__grey-out--animate-out')
       //  .removeClass('shop-smile__grey-out--show');
-      addShaddow();
+      //addShaddow();
     });
   }
   closeSmileWidget();
@@ -68,12 +68,12 @@ $(document).ready(function () {
         console.log('show');
         smileWrapper.removeClass('shop-smile__wrapper--slide-in');
         smileWrapper.removeClass('shop-smile__wrapper--show');
-        addShaddow();
+        //addShaddow();
         smileWrapper.addClass('shop-smile__wrapper--slide-out');
       } else {
         console.log('NO');
         smileWrapper.removeClass('shop-smile__wrapper--slide-out');
-        removeShaddow();
+        //removeShaddow();
         animateIn();
       }
     });
