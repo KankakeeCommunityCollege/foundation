36c380ade743fc012cefe97e503e54afb26e05d2
wdzajicek <tig.welder4@gmail.com>
2018-10-18 14:09:55 -0500

    Custom JS for thumbnail/carousel gallery.

--- assets/js/script/sliders.js
+++ assets/js/script/sliders.js
@@ -1,5 +1,16 @@
 // Custom JS for Slick img carousels
 $(document).ready(function() {
+
+  $('.hero-slider__slider').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow:'<img alt="" class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
+    nextArrow:'<img alt="" class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
+  });
+
   $('.gallery__button').each(function() {
     var that = $(this);
     var slideTarget = that.data('slide');
