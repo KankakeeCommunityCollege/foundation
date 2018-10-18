b49fe4e1b027209741b75859a0f1ebd7ddbba1c7
wdzajicek <tig.welder4@gmail.com>
2018-10-18 13:43:29 -0500

    Create image gallery of event photos...
    Image gallery with thumbnails that open a modal with a slider/carousel

--- assets/js/script/sliders.js
+++ assets/js/script/sliders.js
@@ -1,11 +1,18 @@
 // Custom JS for Slick img carousels
-
-$('.hero-slider__slider').slick({
-  dots: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  prevArrow:'<img alt="" class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
-  nextArrow:'<img alt="" class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
+$(document).ready(function() {
+  $('.gallery__button').each(function() {
+    var that = $(this);
+    var slideTarget = that.data('slide');
+    that.on('click', function() {
+      setTimeout(function(){
+        $('#gallerySlider').slick({
+          dots: true,
+          initialSlide: that.data('slide'),
+          slidesToShow: 1,
+          slidesToScroll: 1
+        });
+      }, 300);
+      $('#gallerySlider').slick('slickGoTo', slideTarget );
+    });
+  });
 });
