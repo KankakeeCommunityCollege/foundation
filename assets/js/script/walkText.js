2427b2f8f9c1f0b3255b64ccd03bf9183f70ef26
wdzajicek <tig.welder4@gmail.com>
2019-07-31 10:04:59 -0500

    Change walktext JS to swap for literal '—' not '&mdash;'.

--- assets/js/script/walkText.js
+++ assets/js/script/walkText.js
@@ -1,7 +1,7 @@
 // Replace all occurences of "--" (double hyphens) with "—" (long-em dash)
 function walkText(node) {
   if (node.nodeType == 3) {
-    node.data = node.data.replace(/--/g, '&mdash;');
+    node.data = node.data.replace(/--/g, '—');
   }
   if (node.nodeType == 1 && node.nodeName != 'SCRIPT') {
     for (var i = 0; i < node.childNodes.length; i++) {
