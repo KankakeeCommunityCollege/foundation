3fac85ae27975f289bd3ae4a085d1931f81e982a
wdzajicek <tig.welder4@gmail.com>
2019-08-20 11:51:04 -0500

    Add New Beginnings content & styling.

--- assets/js/script/accordian.js
+++ assets/js/script/accordian.js
@@ -2,79 +2,58 @@
 // Allows anchor jump-link to work with with acordian function
 //
 
-$('.donateTarget').on('click', function() {
-  $('#collapseOne').toggle('show').trigger('showCollapsed');
+$('#collapseOne').on('show.bs.collapse', function () {
+  $('#plusMinus1').toggleClass('buttons__minus');
 });
 
-$('#collapseOne').on('showCollapsed', function() {
-  $('#plusMinus').toggleClass('buttons__minus');
-});
-
-$('.plannedTarget').on('click', function() {
-  $('#collapseTwo').toggle('show').trigger('showCollapsed2');
-});
-
-$('#collapseTwo').on('showCollapsed2', function() {
+$('#collapseTwo').on('show.bs.collapse', function () {
   $('#plusMinus2').toggleClass('buttons__minus');
 });
 
-$('.womensCircleTarget').on('click', function() {
-  $('#collapseThree').toggle('show').trigger('showCollapsed3');
-});
-
-$('#collapseThree').on('showCollapsed3', function() {
+$('#collapseThree').on('show.bs.collapse', function () {
   $('#plusMinus3').toggleClass('buttons__minus');
 });
 
-$('.employeeTarget').on('click', function() {
-  $('#collapseFour').toggle('show').trigger('showCollapsed4');
-});
-
-$('#collapseFour').on('showCollapsed4', function() {
+$('#collapseFour').on('show.bs.collapse', function () {
   $('#plusMinus4').toggleClass('buttons__minus');
 });
 
-$('.rightsTarget').on('click', function() {
-  $('#collapseFive').toggle('show').trigger('showCollapsed5');
-});
-
-$('#collapseFive').on('showCollapsed5', function() {
+$('#collapseFive').on('show.bs.collapse', function () {
   $('#plusMinus5').toggleClass('buttons__minus');
 });
 
-$('.feesTarget').on('click', function() {
-  $('#collapseSix').toggle('show').trigger('showCollapsed6');
-});
-
-$('#collapseSix').on('showCollapsed6', function() {
+$('#collapseSix').on('show.bs.collapse', function () {
   $('#plusMinus6').toggleClass('buttons__minus');
 });
 
-// function toggleAccordian() {
-//   var plusMinus = document.getElementById('plusMinus');
-//   if (plusMinus.firstChild.nodeValue == '+') {
-//     plusMinus.firstChild.nodeValue = '-';
-//   } else {
-//     plusMinus.firstChild.nodeValue = '+';
-//   }
-// }
+$('#collapseSeven').on('show.bs.collapse', function () {
+  $('#plusMinus7').toggleClass('buttons__minus');
+});
 
-// var member = document.getElementById('plusButton');
-// member.addEventListener('click', toggleAccordian, false);
+$('#collapseOne').on('hide.bs.collapse', function () {
+  $('#plusMinus1').toggleClass('buttons__minus');
+});
 
-// $('#my-link2').click(function() {
-//   $('#collapseTwo').collapse('hide');
-//   $('#collapseTwo').collapse('show');
-// });
+$('#collapseTwo').on('hide.bs.collapse', function () {
+  $('#plusMinus2').toggleClass('buttons__minus');
+});
 
-// function toggleAccordian2() {
-//   var plusMinus = document.getElementById('plusMinus2');
-//   if (plusMinus.firstChild.nodeValue == '+') {
-//     plusMinus.firstChild.nodeValue = '-';
-//   } else {
-//     plusMinus.firstChild.nodeValue = '+';
-//   }
-// }
+$('#collapseThree').on('hide.bs.collapse', function () {
+  $('#plusMinus3').toggleClass('buttons__minus');
+});
 
-// var trainer = document.getElementById('plusButton2');
-// trainer.addEventListener('click', toggleAccordian2, false);
+$('#collapseFour').on('hide.bs.collapse', function () {
+  $('#plusMinus4').toggleClass('buttons__minus');
+});
+
+$('#collapseFive').on('hide.bs.collapse', function () {
+  $('#plusMinus5').toggleClass('buttons__minus');
+});
+
+$('#collapseSix').on('hide.bs.collapse', function () {
+  $('#plusMinus6').toggleClass('buttons__minus');
+});
+
+$('#collapseSeven').on('hide.bs.collapse', function () {
+  $('#plusMinus7').toggleClass('buttons__minus');
+});
