5c7709cad6b09ab3fbba746980ac33d6590ee3fa
wdzajicek <tig.welder4@gmail.com>
2019-10-04 15:01:53 -0500

    Create image gallery for new beginnings.

--- assets/js/script/sliders.js
+++ assets/js/script/sliders.js
@@ -1,29 +1,38 @@
 // Custom JS for Slick img carousels
 $(document).ready(function() {
-
-  $('.hero-slider__slider').slick({
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    prevArrow:'<img alt="Previous slide" class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
-    nextArrow:'<img alt="Next slide" class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
-  });
-
-  $('.gallery__button').each(function() {
-    var that = $(this);
-    var slideTarget = that.data('slide');
-    that.on('click', function() {
-      setTimeout(function(){
-        $('#gallerySlider').slick({
-          dots: true,
-          initialSlide: that.data('slide'),
-          slidesToShow: 1,
-          slidesToScroll: 1
-        });
-      }, 300);
-      $('#gallerySlider').slick('slickGoTo', slideTarget );
+  if ( document.querySelector('.hero-slider__slider') ) {
+    $('.hero-slider__slider').slick({
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      prevArrow:'<img alt="Previous slide" class="a-left control-c prev slick-prev" src="/assets/img/dbl-prev.svg">',
+      nextArrow:'<img alt="Next slide" class="a-right control-c next slick-next" src="/assets/img/dbl-next.svg">'
     });
-  });
+  }
+  if ( document.querySelector('#galleryTrack') ) {
+    $('#galleryTrack').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      prevArrow:'<img alt="Previous slide" class="a-left control-c prev slick-prev" src="/assets/img/dbl-prev.svg">',
+      nextArrow:'<img alt="Next slide" class="a-right control-c next slick-next" src="/assets/img/dbl-next.svg">',
+      asNavFor: '#galleryNav'
+    });
+    $('#galleryNav').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      prevArrow:'<img alt="Previous slide" class="a-left control-c prev slick-prev" src="/assets/img/dbl-prev.svg">',
+      nextArrow:'<img alt="Next slide" class="a-right control-c next slick-next" src="/assets/img/dbl-next.svg">',
+      asNavFor: '#galleryTrack',
+      centerMode: true,
+      focusOnSelect: true
+    });
+  }
 });
