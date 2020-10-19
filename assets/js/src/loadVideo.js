10171805c2dd45c875b7ad155ef9fb39f2b9f1e0
wdzajicek <tig.welder4@gmail.com>
2020-10-19 12:41:06 -0500

    Add employee giving video...
    - **Make `video-header.html` & `loadVideo.js` components reusable on other pages.**
    - Upload the MP4 file to `https://cdn.kcc.edu/foundation/` (same folder as the other video for GIVE DAY)
    - Add Dr. Boyd's video configuration to lazily load the video with the new & improved
    video-header component
    
    Instead of writing code from scratch to play Dr. Boyd's Employee Giving video:
    
    1. Add `video_youtube_share:` key to pages with videos (set to value of the video's
    youtube share link URL e.g. `https://youtu.be/...`)
    2. Make minor modificatios to `video-header.html` to make reusable:
      - Add a data-youtube="https://youtu.be/<video_key>" attribute to grab youtube URL in JS
      - Adjust the share-video-modal: set input value to whatever the video-page's YouTube URL is
    3. Adjust `loadVideo.js` to grab the youtube URL from the above data attribute - Instead of
    the youtube URL being hard-coded into the JS

--- assets/js/src/loadVideo.js
+++ assets/js/src/loadVideo.js
@@ -7,43 +7,45 @@ function capturePerformanceTime() {
   return performance.now();
 }
 
-function addVideoOptionButtons(html) {
+function addVideoOptionButtons(html, mp4) {
   return html += `
     <p>Your browser doesn't support HTML5 video. Here is
-     a <a href="https://cdn.kcc.edu/foundation/kcc-give-day_1080p.mp4">link to the video</a> instead.</p>
+     a <a href="${ mp4 }">link to the video</a> instead.</p>
   </video>
   <div class="d-flex flex-column justify-content-center align-items-center video__play-button--wrapper">
     <button class="video__play-button video__play-button--bg" role="button" id="playButton">Play Video</button>
   </div>`;
 }
 
-function createSourceElements(html, videoArr, typeArr) {
+function createSourceElements(html, videoArr, typeArr, youtubeSource) {
+  let mp4 = videoArr[0];
+
   videoArr.forEach((src,i) => {
     html += `<source src="${src}" type="${typeArr[i]}">`;
     return html;
   });
-  return html = addVideoOptionButtons(html);
+  return html = addVideoOptionButtons(html, mp4);
 }
 
 function createVideoElement(html, videoData) {
   const videoElClasses = ['width__full', 'video'];
 
   html = `<video controls poster="${videoData.poster}" id="videoElement" class="${videoElClasses.join(' ')}">`;
-  return html = createSourceElements(html, videoData.videos.split(','), videoData.videoTypes.split(','));
+  return html = createSourceElements(html, videoData.videos.split(','), videoData.videoTypes.split(','), videoData.youtube);
 }
 
-function createVideoFailMessage() {
+function createVideoFailMessage(ytLink) {
   let message;
   return message = `
   <div class="video__message d-flex flex-column mx-3 my-3 justify-content-center align-items-center">
     <p class="video__typography--whoops mb-0">Whoops!</p>
     <p class="typography__larger-p video__typography--p mb-1 px-3">Something went wrong while loading the video.</p>
-    <p class="typography__larger-p video__typography--p px-3">You may want to <a target="_blank" rel="noopener noreferrer" href="https://youtu.be/mbqJUT89-AY">watch the GIVE DAY video on YouTube</a> instead.</p>
+    <p class="typography__larger-p video__typography--p px-3">You may want to <a target="_blank" rel="noopener noreferrer" href="${ ytLink }">watch the video on YouTube</a> instead.</p>
   </div>`;
 }
 
-function createErrorMessage(videoContianer) {
-  videoContainer.parentElement.innerHTML = `${createVideoFailMessage()}`;
+function createErrorMessage(videoContianer, ytLink) {
+  videoContainer.parentElement.innerHTML = `${createVideoFailMessage(ytLink)}`;
   return console.error('Slow network speeds. Aborting video load');
 }
 
@@ -53,15 +55,15 @@ function createVideo(videoContainer, html) {
   watchForVideoPlay();
 }
 
-function buildVideoOnPageLoad(t0, videoContainer, html) {
+function buildVideoOnPageLoad(t0, videoContainer, ytLink, html) {
   window.addEventListener('load', () => {
 
     const t1 = capturePerformanceTime();
     const slowNetworkSpeeds = t1 - t0 > 6000;
 
     slowNetworkSpeeds ?
-      createErrorMessage(videoContainer)
-      : createVideo(videoContainer, html);
+      createErrorMessage(videoContainer, ytLink)
+    : createVideo(videoContainer, html);
   });
 }
 
@@ -71,10 +73,12 @@ function loadVideo() {
   if ( ! document.getElementById(VIDEO_PLACEHOLDER_ID) )
     return;
 
+  const PLACEHOLDER = document.getElementById(VIDEO_PLACEHOLDER_ID);
   const t0 = capturePerformanceTime();
-  let html = createVideoElement(html, document.getElementById(VIDEO_PLACEHOLDER_ID).dataset);
+  let html = createVideoElement(html, PLACEHOLDER.dataset);
+  let YOUTUBE_LINK = PLACEHOLDER.dataset.youtube;
 
-  buildVideoOnPageLoad(t0, document.getElementById(VIDEO_CONTAINER_ID), html);
+  buildVideoOnPageLoad(t0, document.getElementById(VIDEO_CONTAINER_ID), YOUTUBE_LINK, html);
 }
 
 export default loadVideo;
