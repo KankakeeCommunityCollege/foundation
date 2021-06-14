8fc9b2034df92bd8eb7f080eee90ab6c6e89be62
wdzajicek <tig.welder4@gmail.com>
2021-06-14 17:03:13 -0500

    Add SVG icon animation for mobiles users...
    Use the Intersection Observer API on mobile devices to animate the SVG
    icons when they are scrolled into view.

---
+++ assets/js/src/animateIcons.js
@@ -0,0 +1,27 @@
+const SVG_QUERY = 'svg';
+let intersectionOptions = {
+  threshold: 0.9
+}
+
+function addAnimation(el, observer) {
+  el.classList.add('animateMobile');
+  observer.disconnect();
+  return observer.unobserve(el);
+}
+
+function animateIcons() {
+  const svgList = document.querySelectorAll(SVG_QUERY);
+  let callback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.intersectionRatio != 1 && entry.intersectionRatio > 0) {
+        entry.isIntersecting == true ? addAnimation(entry.target, observer) : null;
+      }
+    });
+  };
+  [...svgList].forEach(svg => {
+    let observer = new IntersectionObserver(callback, intersectionOptions);
+    observer.observe(svg);
+  });
+}
+
+export default animateIcons;
