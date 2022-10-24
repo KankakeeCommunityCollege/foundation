de083c59a0d10e89fd410ff3f208abe68bb98339
wdzajicek <tig.welder4@gmail.com>
2022-10-24 13:00:51 -0500

    Update employee giving page for 2023.

--- assets/js/src/checkScholarshipApp.js
+++ assets/js/src/checkScholarshipApp.js
@@ -11,9 +11,15 @@ const openMessage = `<p class="mb-4">
       Apply for Scholarships</a>
   </p>
   <div style="max-width: 800px;" class="alert alert-info mx-auto mb-5 text-left">
-    <p class="mb-2"><strong>Awards will be announced on May 15.</strong> Applicants will be notified via email.
-    Scholarship recipients must be enrolled at KCC for the 2023-24 academic year to receive award.</p>
-    <p class="mb-0 text-center"><strong>Need Help?</strong> Contact the foundation office: <a href="tel:+18158028950">815-802-8950</a> or <a href=""mailto:kccfoundation@kcc.edu>kccfoundation@kcc.edu</a></p>
+    <p class="mb-2">
+      <strong>Awards will be announced on May 15.</strong>
+      Applicants will be notified via email.
+      Scholarship recipients must be enrolled at KCC for the 2023-24 academic year to receive award.
+    </p>
+    <p class="mb-0 text-center">
+      <strong>Need Help?</strong> Contact the foundation office: 
+      <a href="tel:+18158028950">815-802-8950</a> or <a href=""mailto:kccfoundation@kcc.edu>kccfoundation@kcc.edu</a>
+    </p>
   </div>`;
 const closedMessage = `<p class="mb-4">
   <strong>The application is not open yet.</strong></p>`;
